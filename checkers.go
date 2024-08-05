b8d6d50333d42213f01fe6e4d91c08059f8fb26a
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-08-05 17:55:17 +0500

    add window icon load function

--- checkers.go
+++ checkers.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"log"
+	"image"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// Loads icon from file.
+func loadIcon() image.Image {
+	file, err := os.Open("assets/icon.png")  // Image created with flaticon.com
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return img
+}
+
 func main() {
 	game := NewGame()
+	winIcon := loadIcon()
 
 	ebiten.SetWindowSize(winWidth, winHeight)
 	ebiten.SetWindowTitle("Checkers")
+	ebiten.SetWindowIcon([]image.Image{winIcon})
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
